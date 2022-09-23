d7b3e2d892e5015efffb122cd72ffc88f09f3f0d
paulavrodriguezc <r.paula@correo.unimet.edu.ve>
2022-09-23 14:55:23 -0400

    Ejercicios

--- Ejercicios-repaso/Ejercicio 7.py
+++ Ejercicios-repaso/Ejercicio 7.py
@@ -1,9 +1,14 @@
 number=input("Please enter an integer number greater than 0: ")
+count = 0
 if number.isnumeric():
     number=int(number)
     if number>0:
         for i in range(number, -1, -1):
-            print(i, end=", ")
+            if count!=number:
+                print(i, end=", ")
+            else:
+                print(i)
+            count+=1
     else:
         print(f"The number {number} is not greater than 0.")
 else:
