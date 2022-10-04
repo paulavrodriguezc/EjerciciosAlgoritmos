fe9fa635cd4e4cd57d45ac99d90e74b61f883bf6
paulavrodriguezc <r.paula@correo.unimet.edu.ve>
2022-10-03 23:14:50 -0400

    Ejercicios

--- Ejercicios-repaso/Ejercicio 18.py
+++ Ejercicios-repaso/Ejercicio 18.py
@@ -1,8 +1,6 @@
 number=input("Please enter an integer number: ")
 while not number.isnumeric():
-    print("Invalid input.")
-    number=input("Please enter an integer number: ")
-#primo o compuesto
+    number=input("Invalid input. Please enter an integer number: ")
 number=int(number)
 aux=0
 for i in range(1,number+1,1):
@@ -12,7 +10,6 @@ if aux==2:
     print(f"The number {number} is prime.")
 else:
     print(f"The number {number} is composite.")
-#oblongo
 is_pronic=False
 for i in range(1,number+1):
     if number==(i)*(i+1):
@@ -21,8 +18,7 @@ if is_pronic:
     print(f"The number {number} is pronic.")
 else:
     print(f"The number {number} is not pronic.")        
-#palíndromo (cómo recorro número por número el número completo y cómo los comparo)
-#perfecto, abundante o deficiente
+#palíndromo (cómo recorro número por número el número completo y cómo los comparo/hago que los dígitos se guarden en una lista? y después comparo?)
 count=0
 for x in range(1,number):
     if number%x==0:
@@ -33,7 +29,6 @@ elif count>number:
     print(f"The number {number} is abundant.")
 else:
     print(f"The number {number} is deficient.")
-#cuadrado o libre de cuadrados
 square=number**(1/2)
 if square/int(square)==1:
     print(f"The number {number} is a square.")
