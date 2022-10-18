bb2604e94a50e04ca76a1ebd73a2d3fc2e8f2adb
paulavrodriguezc <r.paula@correo.unimet.edu.ve>
2022-10-17 22:06:43 -0400

    Ejercicios

--- Semana 5/Clase 1 (Ej. 1).py
+++ Semana 5/Clase 1 (Ej. 1).py
@@ -16,10 +16,10 @@ def get_client_data(study):
     return client
 def print_invoice(client,studies):
     print("***RECEIPT***")
-    print("ID number: ", client.get["ID"])
-    print("Age ", client.get["age"])
-    print("Sex: ", client.get["sex"])
-    print("Study in question: ", studies.get(client.get["study"]))
+    print("ID number: ", client.get("ID"))
+    print("Age: ", client.get("age"))
+    print("Sex: ", client.get("sex"))
+    print("Study in question: ", studies.get(client.get("study")))
 def discounts_gotten(client, studies, client_number):
     discount_applied=0
     if client.get("Sex")=="F" and client.get("Age")>55:
@@ -57,6 +57,9 @@ def main():
     clients_t=0
     clients_r=0
     average_per_client=0
+    total_u=0
+    total_t=0
+    total_r=0
     average_u=0
     average_t=0
     average_r=0
@@ -72,21 +75,33 @@ def main():
         print_invoice(client,studies)
         if client.get("study")=="U":
             clients_u+=1
+            total_u+=total_client
         elif client.get("study")=="T":
             clients_t+=1
+            total_t+=total_client
         else:
             clients_r+=1
+            total_r+=total_client
         if len(clients)>=1:
             average_per_client=(total_day)/(len(clients))
         else:
             print("There were no clients today.")
         if clients_u>=1:
-            pass
+            average_u=total_u/clients_u
         if clients_t>=1:
-            pass
+            average_t=total_t/clients_t
         if clients_r>=1:
-            pass
+            average_r=total_r/clients_r
         if input("Do you wish to continue? \nY for yes. \nN for no. \n---> ")=="N":
             break
-        # print_end_day(promedio de pago por tipo de estudio)
+        print("***END OF DAY***")
+        print(f"\tTotal clients in U: {clients_u}.")
+        print(f"\tTotal clients in T: {clients_t}.")
+        print(f"\tTotal clients in R: {clients_r}.")
+        print(f"\tThe amount of money discounted: ${total_discounts}.")
+        print(f"\tAverage pay per client: ${average_per_client}.")
+        print(f"\tAverage pay per client in U: ${average_u}.")
+        print(f"\tAverage pay per client in T: ${average_t}.")
+        print(f"\tAverage pay per client in R: ${average_r}.")
+        print(f"\tTOTAL OF THE DAY: ${total_day}.")
 main()
