cc4d02499c6fa89ac719d4b8186a3335773d8eb7
paulavrodriguezc <r.paula@correo.unimet.edu.ve>
2022-11-04 09:50:01 -0400

    Ejercicios

--- Semana 7/Main Prepas 2.py
+++ Semana 7/Main Prepas 2.py
@@ -1,6 +1,8 @@
 # hacer funciones de validación de alpha, num y opciones
 # hacer función para crear employee. después dependiendo de si es developer, accountant y HR, pedir su atributo especial (input de tipo)
+# corroborar que el id no esté ya en los empleados
 # añadir al diccionario y a la lista cada empleado añadido: edificio["Recursos Humanos"].append(HR(parámetros))
 # función para pedir si se desea seguir añadiendo
 # función para imprimir departamento y empleados (una en main y otra en la clase de Employee para devolver SOLO los datos
 # y NO la posición)
+# función para remover un empleado en base a su cédula
